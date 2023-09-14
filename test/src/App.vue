8b1873676154ea002c04624ca4842acae092cd82
kaynekaynekayne <theygoon@gmail.com>
2023-09-15 06:44:12 +0900

    m

--- test/src/App.vue
+++ test/src/App.vue
@@ -11,20 +11,13 @@
 import Header from './components/layout/Header.vue'
 
 export default {
-  name: 'App',
-  components: {
-    Header,
-  }
+  // name: 'App',
+  // components: {
+  //   Header,
+  // }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- test/src/main.js
+++ test/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import router from './router';
 
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
@@ -15,4 +16,5 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')
